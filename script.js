c1beb35efa0b1bbe7b90871e306aaea7ea0069d0
unknown <nguyenduongtrung0803@gmail.com>
2024-04-30 20:28:52 +0700

    hi

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = 'Liệu em có còn cơ hội nào không ạ ?';
+    document.getElementById('messageContainer').innerHTML = 'Em hứa sẽ không tái phạm nửaa';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
@@ -261,4 +261,3 @@ document.getElementById('noBtn').addEventListener('click', function() {
             break;
     }
 });
-
